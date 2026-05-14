2f7fba5b48abed763f9533f360ae21b1b3c15db3
Hammas-shahzad <hammas.sheikh85@gmail.com>
2026-05-14 11:32:50 +0500

    fix production config

--- app.py
+++ app.py
@@ -272,4 +272,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
